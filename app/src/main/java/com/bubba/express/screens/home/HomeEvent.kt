17c54f809919fc689a2e64089759d31872bca5a9
Diegom3d <Diegom3d@users.noreply.github.com>
2025-12-10 00:09:09 -0600

    objetos de eventos en home clicks de pantallas

--- app/src/main/java/com/bubba/express/screens/home/HomeEvent.kt
+++ app/src/main/java/com/bubba/express/screens/home/HomeEvent.kt
@@ -3,5 +3,8 @@ package com.bubba.express.screens.home
 sealed class HomeEvent {
     object LoadProducts : HomeEvent()
     data class OnProductClick(val productId: Int) : HomeEvent()
-
+    object OnCartClick : HomeEvent()
+    object OnProfileClick : HomeEvent()
+    object OnLogoutClick : HomeEvent()
+    object OnHistoryClick : HomeEvent()
 }
