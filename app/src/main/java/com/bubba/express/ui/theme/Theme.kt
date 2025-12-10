5d4335d2ce8591585e8e42d8f67e11c1688d2013
Diegom3d <Diegom3d@users.noreply.github.com>
2025-12-09 23:28:46 -0600

    variable lightColor

--- app/src/main/java/com/bubba/express/ui/theme/Theme.kt
+++ app/src/main/java/com/bubba/express/ui/theme/Theme.kt
@@ -1,8 +1,8 @@
 package com.bubba.express.ui.theme
 
 
-
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 
 private val DarkColorScheme = darkColorScheme(
     primary = CoffeePrimary,
@@ -15,6 +15,18 @@ private val DarkColorScheme = darkColorScheme(
     onTertiary = Black,
     onBackground = CreamBackground,
     onSurface = CreamBackground
+)
+private val LightColorScheme = lightColorScheme(
+    primary = CoffeePrimary,
+    secondary = CoffeeSecondary,
+    tertiary = CoffeeAccent,
+    background = CreamBackground,
+    surface = WhitePure,
+    onPrimary = WhitePure,
+    onSecondary = WhitePure,
+    onTertiary = Black,
+    onBackground = Black,
+    onSurface = Black
 )
 
 
