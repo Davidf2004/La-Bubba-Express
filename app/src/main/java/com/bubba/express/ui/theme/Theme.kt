b48fe8235a741e5430f5a0662730ec899ce28e10
Diegom3d <Diegom3d@users.noreply.github.com>
2025-12-09 23:35:09 -0600

    tema personalizado para Jetpack Compose usando Material 3.

--- app/src/main/java/com/bubba/express/ui/theme/Theme.kt
+++ app/src/main/java/com/bubba/express/ui/theme/Theme.kt
@@ -1,6 +1,13 @@
 package com.bubba.express.ui.theme
 
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 
@@ -29,4 +36,25 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Black
 )
 
+@Composable
+fun LaBubbaExpressTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
 
